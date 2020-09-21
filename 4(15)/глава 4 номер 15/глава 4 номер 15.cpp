d94d6321e4cb3aeb3c38bef901fe596500337ec1
IvanLevakin <levakin99@mail.ru>
2020-09-21 09:44:11 +0500

    dz glavi 3-5 ypr 15

---
+++ 4(15)/глава 4 номер 15/глава 4 номер 15.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+bool check(int number) {
+    for (int i = 2; i < number; i++)
+        if (number % i == 0)
+            return false;
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 2; i < n; i++)
+        if (check(i) == true)
+            cout << i << " ";
+
+}
+
+
